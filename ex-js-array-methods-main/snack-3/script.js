1b9e0b5ed3c8201f9974ba395422fdf7affc2017
matteovecchio04 <matteovex@gmail.com>
2025-10-22 15:59:50 +0200

    snack 3

--- ex-js-array-methods-main/snack-3/script.js
+++ ex-js-array-methods-main/snack-3/script.js
@@ -1,4 +1,8 @@
 const numbers = [2, 8, 4, 7, 2, 87];
 
+const plusOne = numbers.map(n => n + 1)
+// metto map perchè nel risultato si deve vedere un array nuovo
+console.log(plusOne);
+
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
