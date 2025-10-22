7dde3f73d5f653f38125bb2ce0b9c1fdf04b0993
matteovecchio04 <matteovex@gmail.com>
2025-10-22 17:22:57 +0200

    snack 5

--- ex-js-array-methods-main/snack-5/script.js
+++ ex-js-array-methods-main/snack-5/script.js
@@ -1,4 +1,9 @@
 const nums = [2, 8, 4, 7, 12, 87];
 
+const even = nums.filter(n => n % 2 == 0)
+
+console.log(even);
+
+
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
